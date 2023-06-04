f4c52f0d3ee779c71772ab19652385a25705b44c
Oscar Soanes <oscarsoanes@gmail.com>
2023-06-04 12:17:20 +0100

    Added support for mongodb to store messages

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const logger = require('morgan');
 const indexRouter = require('./routes/index');
 const newMessageRouter = require('./routes/new');
 
+
 const app = express();
 
 // view engine setup
